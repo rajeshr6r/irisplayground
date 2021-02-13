0290cea399951890a2f488763e2d5cb8df7cac0b
Rajesh Rajamani <rajesh.cloud.1@outlook.com>
2021-02-13 20:10:02 +0530

    host IP changes in docker

--- predictionapp.py
+++ predictionapp.py
@@ -48,5 +48,5 @@ def healthcheck():
     return jsonify("Performed HealthCheck.Container is loading fine ")
 
 if __name__ == '__main__':
-  app.run(host='127.0.0.1', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=8000, debug=True)
  
